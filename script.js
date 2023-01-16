f08a217f594df3199492018eef9d8dedff724d3e
msoeker <80034462+msoeker@users.noreply.github.com>
2023-01-16 21:54:26 +0100

    add playGame function to play over 5 rounds

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ function getPlayerSelection() {
   return choice.toLowerCase();
 }
 
-function playRound(playerSelection, AIChoice, playerScore, AIScore){
+function playRound(playerSelection, AIChoice){
   console.log(`Player picked ${playerSelection} and AI picked ${AIChoice}`)
   if(playerSelection == "rock"){ 
-    if(AIChoice == "scissors") playerScore++
-    if(AIChoice == "paper") AIScore++
+    if(AIChoice == "scissors") return true
+    if(AIChoice == "paper") return false
     if(AIChoice == "rock") return "draw"
   }
   if(playerSelection == "scissors"){
     if(AIChoice == "scissors") return "draw"
-    if(AIChoice == "paper") playerScore++
-    if(AIChoice == "rock") AIScore++
+    if(AIChoice == "paper")  return true
+    if(AIChoice == "rock") return false
   }
   if(playerSelection == "paper"){
-    if(AIChoice == "scissors") return AIScore++
-    if(AIChoice == "paper") return
-    if(AIChoice == "rock") return  playerScore++
+    if(AIChoice == "scissors") return false
+    if(AIChoice == "paper") return "draw"
+    if(AIChoice == "rock") return true
   }
   return
 }
@@ -32,15 +32,28 @@ function playRound(playerSelection, AIChoice, playerScore, AIScore){
 function playGame(){
   let playerScore = 0
   let AIScore = 0
-  for(let i = 0; i<=5; i++){
+  let result
+  for(let i = 1; i<=5; i++){
+    console.log(`Round ${i}! Let's go!`)
+    result = playRound(getPlayerSelection(), getAIChoice())
+    if (result == "draw"){
+      console.log("DRAW! Play again!")
+    }else if(result){
+      playerScore++
+      console.log("Round goes to Player!")
+    }else{
+      AIScore++
+      console.log("Round goes to AI!")
+    }
     console.log(`Player: ${playerScore}`)
     console.log(`AI: ${AIScore}`)
-    playRound(getPlayerSelection(), getAIChoice(), playerScore, AIScore);
-    if(playerScore == 5 || AIScore == 5){
-      playerScore == 5 ? console.log("Yeah Player wins!") : console.log("AI wins!")
-      return
-    }
   }
+  if(playerScore==AIScore){
+    console.log("DRAW! No one wins the game")
+  }else{
+    playerScore > AIScore ? console.log("Yeah Player wins!") : console.log("AI wins!")
+  }
+  return
 }
 
 playGame();
